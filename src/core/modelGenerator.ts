bc781e033a6efe7aa854487b1a43da019fcd46e2
wangancheng <wangancheng@yxtreme.cn>
2022-02-28 23:21:54 +0800

    feat: imports

--- src/core/modelGenerator.ts
+++ src/core/modelGenerator.ts
@@ -1,18 +1,15 @@
 export interface ModelDescribe {
-  vertices: Array<Float32Array>,
-  indices: Array<number>,
+  vertices: Array<Float32Array>;
+  indices: Array<number>;
 }
 
 export interface CubeSides {
-  long: number,
-  width: number,
-  height: number,
+  long: number;
+  width: number;
+  height: number;
 }
 
-function cubeModelGenerator(
-  centerPosition: Float32Array,
-  sides: CubeSides,
-): ModelDescribe {
+function cubeModelGenerator(centerPosition: Float32Array, sides: CubeSides): ModelDescribe {
   const c = { x: centerPosition[0], y: centerPosition[1], z: centerPosition[2] };
   const half = { w: sides.width / 2, l: sides.long / 2, h: sides.height / 2 };
 
@@ -28,21 +25,27 @@ function cubeModelGenerator(
   return {
     vertices: [v1, v2, v3, v4, v5, v6, v7, v8],
     indices: [
-      [0, 3, 7], [7, 4, 0], // 前
-      [1, 2, 6], [6, 5, 1], // 后
-      [0, 1, 5], [5, 4, 0], // 左
-      [3, 2, 6], [3, 7, 6], // 右
-      [0, 1, 2], [0, 3, 2], // 上
-      [4, 5, 6], [4, 7, 6], // 下
+      [0, 3, 7],
+      [7, 4, 0], // 前
+      [1, 2, 6],
+      [6, 5, 1], // 后
+      [0, 1, 5],
+      [5, 4, 0], // 左
+      [3, 2, 6],
+      [3, 7, 6], // 右
+      [0, 1, 2],
+      [0, 3, 2], // 上
+      [4, 5, 6],
+      [4, 7, 6], // 下
     ],
   };
 }
 
 function dataGenerator(model: ModelDescribe) {
-  const vertices_2d = (model.indices.flat()).map((index) => model.vertices[index]);
+  const vertices_2d = model.indices.flat().map((index) => model.vertices[index]);
   const indices = vertices_2d.map((_, idx) => idx);
   return {
-    vertices: vertices_2d.flat(),
+    vertices: new Float32Array(vertices_2d.flat()),
     indices,
   };
 }
--- src/index.ts
+++ src/index.ts
@@ -19,15 +19,25 @@ const gl = webgl.create(document.getElementById("glcanvas") as HTMLCanvasElement
 gl.clearColor(0.0, 0.0, 0.0, 1.0);
 gl.enable(gl.DEPTH_TEST);
 
-const cubeModel = mg.dataGenerator(mg.cubeModelGenerator([0, 0, 0], { long: 0.5, width: 0.5, height: 0.5 }));
-cubeModel.textureVertices = [
-  [0.0, 1.0, 1.0, 1.0, 1.0, 0.0], [1.0, 0.0, 0.0, 0.0, 0.0, 1.0],
-  [0.0, 1.0, 1.0, 1.0, 1.0, 0.0], [1.0, 0.0, 0.0, 0.0, 0.0, 1.0],
-  [0.0, 1.0, 1.0, 1.0, 1.0, 0.0], [1.0, 0.0, 0.0, 0.0, 0.0, 1.0],
-  [0.0, 1.0, 1.0, 1.0, 1.0, 0.0], [1.0, 0.0, 0.0, 0.0, 0.0, 1.0],
-  [0.0, 1.0, 1.0, 1.0, 1.0, 0.0], [1.0, 0.0, 0.0, 0.0, 0.0, 1.0],
-  [0.0, 1.0, 1.0, 1.0, 1.0, 0.0], [1.0, 0.0, 0.0, 0.0, 0.0, 1.0],
-].flat();
+const cubeModel = mg.dataGenerator(
+  mg.cubeModelGenerator([0, 0, 0], { long: 0.5, width: 0.5, height: 0.5 })
+);
+cubeModel.textureVertices = new Float32Array(
+  [
+    [0.0, 1.0, 1.0, 1.0, 1.0, 0.0],
+    [1.0, 0.0, 0.0, 0.0, 0.0, 1.0],
+    // [0.0, 1.0, 1.0, 1.0, 1.0, 0.0],
+    // [1.0, 0.0, 0.0, 0.0, 0.0, 1.0],
+    // [0.0, 1.0, 1.0, 1.0, 1.0, 0.0],
+    // [1.0, 0.0, 0.0, 0.0, 0.0, 1.0],
+    // [0.0, 1.0, 1.0, 1.0, 1.0, 0.0],
+    // [1.0, 0.0, 0.0, 0.0, 0.0, 1.0],
+    // [0.0, 1.0, 1.0, 1.0, 1.0, 0.0],
+    // [1.0, 0.0, 0.0, 0.0, 0.0, 1.0],
+    // [0.0, 1.0, 1.0, 1.0, 1.0, 0.0],
+    // [1.0, 0.0, 0.0, 0.0, 0.0, 1.0],
+  ].flat()
+);
 console.log("cubeModel: ", cubeModel);
 
 // 设置着色器
@@ -46,7 +56,7 @@ gl.bindVertexArray(vao);
 
 const vbo = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(cubeModel.vertices), gl.STATIC_DRAW);
+gl.bufferData(gl.ARRAY_BUFFER, cubeModel.vertices, gl.STATIC_DRAW);
 
 const positionLocation = gl.getAttribLocation(program, "position");
 gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
@@ -54,20 +64,20 @@ gl.enableVertexAttribArray(positionLocation);
 
 const textureBuffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, textureBuffer);
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(cubeModel.textureVertices), gl.STATIC_DRAW);
+gl.bufferData(gl.ARRAY_BUFFER, cubeModel.textureVertices, gl.STATIC_DRAW);
 
 const textureLocation = gl.getAttribLocation(program, "in_texture");
 gl.vertexAttribPointer(textureLocation, 2, gl.FLOAT, false, 0, 0);
 gl.enableVertexAttribArray(textureLocation);
 
 const matLocation = gl.getUniformLocation(program, "matrix");
-gl.uniformMatrix4fv(matLocation, false, glm.rotateX(90));
+gl.uniformMatrix4fv(matLocation, false, glm.rotateX(0));
 
 const ebo = gl.createBuffer();
 gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ebo);
-gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(cubeModel.indices), gl.STATIC_DRAW)
+gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(cubeModel.indices), gl.STATIC_DRAW);
 
-import("./textures/tan.jpg").then(res => {
+import("./textures/tan.jpg").then((res) => {
   const texture = gl.createTexture();
   gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
   const image = new Image();
@@ -84,7 +94,14 @@ import("./textures/tan.jpg").then(res => {
     gl.uniform1i(textureSampler1, 0);
 
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_bit);
-    
-    gl.drawElements(gl.TRIANGLES, cubeModel.indices.length, gl.UNSIGNED_SHORT, 0);
+
+    // gl.drawElements(gl.TRIANGLES, cubeModel.indices.length, gl.UNSIGNED_SHORT, 0);
+    gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
   };
 });
+
+Promise.all([
+  import("./textures/tan.jpg"),
+  import("./textures/texture2.jpeg"),
+  import("./textures/textureExample.jpeg"),
+]).then((imps) => {});
